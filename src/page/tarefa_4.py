eca259789cc7aadee114a8c39aa544f7b69a556b
Jonathas Vinicius <jonathas.vinicius@ufpe.br>
2024-09-05 01:36:21 +0000

    feat: Atualizar descrição da Tarefa 4 para apresentar o percentual de representação de cada estado a partir do csv carregado
    feat: Visualização da Tarefa 4

--- src/page/tarefa_4.py
+++ src/page/tarefa_4.py
@@ -1,11 +1,11 @@
 """Tela Tarefa 4: Cálculo da Representação Percentual por Estado."""
 
-from pprint import pprint
 import streamlit as st
-# import pandas as pd
-# import altair as alt
+import pandas as pd
+import altair as alt
 from utils.functions.percentual import (
     calcular_percentual,
+    calcular_soma,
 )
 from utils.load import load_dataset, load_toml
 
@@ -19,22 +19,39 @@ def load_data():
 
 
 data = load_data()
-
-
+soma = calcular_soma(data)
 percentual = calcular_percentual(data)
 
-#TODO: Implementar a função de visualização
-#TODO: Implementar Soma dos valores de faturamento dos estados
-
+# Converte os dados percentuais em um DataFrame para visualização
+df_percentual = pd.DataFrame(percentual.items(), columns=['Estado', 'Percentual'])
 
 def Visualize() -> None:
     """Função para obter as visualizações da representação percentual."""
-    pprint(percentual)
+
+    col = st.columns(1)[0]
+    total = soma.get('Total', 0)
+    col.metric("Somatório dos valores de faturamento", f"{total:,.2f}")
+
+    st.subheader("Representação Percentual por Estado (Tabela)")
+    st.table(df_percentual)
+    
+    st.subheader("Representação Percentual por Estado (Gráfico)")
+    chart = alt.Chart(df_percentual).mark_bar().encode(
+        x=alt.X('Estado', sort='-y'),
+        y='Percentual',
+        color='Estado',
+        tooltip=['Estado', 'Percentual']
+    ).properties(
+        width=600,
+        height=400
+    )
+    
+    st.altair_chart(chart)
 
 def show_page() -> None:
     """Mostra a página Tarefa 4."""
     
-    # Título e descrição
+    # Título e descrição da tarefa
     st.title(labels['tarefa_4']['titulo'])
     st.write(labels['tarefa_4']['descricao'])
     
--- src/utils/functions/percentual.py
+++ src/utils/functions/percentual.py
@@ -2,20 +2,26 @@
 
 import streamlit as st
 
-
-
 @st.cache_data
 def calcular_percentual(dados: dict) -> dict:
     """Calcula o percentual de cada valor em relação ao total."""
-    # Calcula o total dos valores
+
     total = dados['Valor'].sum()
     
-    # Calcula o percentual de cada valor em relação ao total
     dados['Percentual'] = (dados['Valor'] / total) * 100
     
-    # Converte para dicionário
     percentuais = dados.set_index('Estado')['Percentual'].to_dict()
     
     return percentuais
 
-#TODO: Implementar Soma dos valores de faturamento dos estados
+@st.cache_data
+def calcular_soma(dados: dict) -> dict:
+    """Calcula a soma dos valores de faturamento dos estados."""
+    
+    soma_total = dados['Valor'].sum()
+    
+    somas = dados.set_index('Estado')['Valor'].to_dict()
+    
+    somas['Total'] = soma_total
+    
+    return somas
